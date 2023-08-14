f561c6d570a1a42520195d49806e2c06bb445233
Grant Gongaware <grant@telmate.com>
2023-08-14 11:05:30 -0700

    Change existing table exception to a log warning. Keep processing.

--- src/main/java/com/zendesk/maxwell/schema/ddl/ResolvedTableCreate.java
+++ src/main/java/com/zendesk/maxwell/schema/ddl/ResolvedTableCreate.java
@@ -3,12 +3,15 @@ package com.zendesk.maxwell.schema.ddl;
 import com.zendesk.maxwell.schema.Database;
 import com.zendesk.maxwell.schema.Schema;
 import com.zendesk.maxwell.schema.Table;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ResolvedTableCreate extends ResolvedSchemaChange {
 	public String database;
 	public String table;
 	public Table def;
 
+	static final Logger LOGGER = LoggerFactory.getLogger(ResolvedTableCreate.class);
 
 	public ResolvedTableCreate() {}
 	public ResolvedTableCreate(Table t) {
@@ -21,8 +24,11 @@ public class ResolvedTableCreate extends ResolvedSchemaChange {
 	public void apply(Schema schema) throws InvalidSchemaError {
 		Database d = schema.findDatabaseOrThrow(this.database);
 
-		if ( d.hasTable(this.table) )
-			throw new InvalidSchemaError("Unexpectedly asked to create existing table " + this.table);
+		if ( d.hasTable(this.table) ) {
+			//throw new InvalidSchemaError("Unexpectedly asked to create existing table " + this.table);
+			LOGGER.warn("Unexpectedly asked to create existing table " + this.database + "." + this.table + ". Will skip statement");
+			return;
+		}
 
 		d.addTable(this.def);
 	}
