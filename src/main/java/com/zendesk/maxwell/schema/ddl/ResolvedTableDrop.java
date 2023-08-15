4c76caffe0d1fe46ac233a8b09169e23af49029b
Grant Gongaware <grant@telmate.com>
2023-08-15 11:14:57 -0700

    Log and skip missing table drops

--- src/main/java/com/zendesk/maxwell/schema/ddl/ResolvedTableDrop.java
+++ src/main/java/com/zendesk/maxwell/schema/ddl/ResolvedTableDrop.java
@@ -2,10 +2,14 @@ package com.zendesk.maxwell.schema.ddl;
 
 import com.zendesk.maxwell.schema.Database;
 import com.zendesk.maxwell.schema.Schema;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ResolvedTableDrop extends ResolvedSchemaChange {
 	public String database;
 	public String table;
+
+	static final Logger LOGGER = LoggerFactory.getLogger(ResolvedTableDrop.class);
 
 	public ResolvedTableDrop() { }
 	public ResolvedTableDrop(String database, String table) {
@@ -15,10 +19,14 @@ public class ResolvedTableDrop extends ResolvedSchemaChange {
 
 	@Override
 	public void apply(Schema schema) throws InvalidSchemaError {
-		Database d = schema.findDatabaseOrThrow(this.database);
-		d.findTableOrThrow(this.table);
+		try {
+			Database d = schema.findDatabaseOrThrow(this.database);
+			d.findTableOrThrow(this.table);
 
-		d.removeTable(this.table);
+			d.removeTable(this.table);
+		} catch(InvalidSchemaError ise)  {
+			LOGGER.warn("Table drop problem " + ise.toString() + ". Will skip statement");
+		}
 	}
 
 	@Override
