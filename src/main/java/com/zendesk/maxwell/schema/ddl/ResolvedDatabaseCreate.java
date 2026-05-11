bfc234d68bc0f38f16cd716ce4c6338f1d0933d6
Grant Gongaware <grant@telmate.com>
2026-05-11 12:57:34 -0400

    Log and skip just like table

--- src/main/java/com/zendesk/maxwell/schema/ddl/ResolvedDatabaseCreate.java
+++ src/main/java/com/zendesk/maxwell/schema/ddl/ResolvedDatabaseCreate.java
@@ -1,11 +1,16 @@
 package com.zendesk.maxwell.schema.ddl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.zendesk.maxwell.schema.Database;
 import com.zendesk.maxwell.schema.Schema;
 
 public class ResolvedDatabaseCreate extends ResolvedSchemaChange {
 	public String database;
 	public String charset;
+
+	static final Logger LOGGER = LoggerFactory.getLogger(ResolvedDatabaseCreate.class);
 
 	public ResolvedDatabaseCreate() { }
 	public ResolvedDatabaseCreate(String database, String charset) {
@@ -15,8 +20,11 @@ public class ResolvedDatabaseCreate extends ResolvedSchemaChange {
 
 	@Override
 	public void apply(Schema schema) throws InvalidSchemaError {
-		if ( schema.hasDatabase(database) )
-			throw new InvalidSchemaError("Unexpectedly asked to create existing database " + database);
+		if ( schema.hasDatabase(database) ) {
+			//throw new InvalidSchemaError("Unexpectedly asked to create existing database " + database);
+			LOGGER.warn("Unexpectedly asked to create existing database " + this.database + ". Will skip statement");
+			return;
+		}
 
 		schema.addDatabase(new Database(database, charset));
 	}
